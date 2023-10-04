99e0040602f76e7591f29542750754b1d1f56368
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2023-10-04 08:29:38 +0800

    Update play2.py

--- play2.py
+++ play2.py
@@ -2,6 +2,7 @@ import pyautogui
 import pygetwindow
 import time
 from PIL import Image
+import numpy as np
 
 def 切換至目標視窗():
     # 視窗標題或部分標題（不區分大小寫），用來指定特定視窗
@@ -40,12 +41,17 @@ def 戰鬥():
     monster_B_path = Image.open('白鱷魚.PNG')
     monster_C_path = Image.open('黑貓咪.PNG')
     monster_D_path = Image.open('黑鱷魚.PNG')
-    
+    not_in_fight_path = Image.open('非戰鬥中.PNG')
+
     # 定義模糊比對參數
     confidence_threshold = 0.8
 
+    # 計數用
+    count = 0
+
     # 循环直到屏幕上不再出现(A, B, C)怪物图像
     while True:
+
         # 搜索怪物A图像
         monster_a_location = pyautogui.locateOnScreen(monster_A_path,confidence=confidence_threshold)
         # 搜索怪物B图像
@@ -54,55 +60,67 @@ def 戰鬥():
         monster_c_location = pyautogui.locateOnScreen(monster_C_path,confidence=confidence_threshold)
         # 搜索怪物D图像
         monster_d_location = pyautogui.locateOnScreen(monster_D_path,confidence=confidence_threshold)
+        # 非戰鬥狀態
+        not_in_fight = pyautogui.locateOnScreen(not_in_fight_path,confidence=confidence_threshold)
+
+        # count >= 1 代表已經使用過1次黑洞爆發則可以用吸血回一下hp
+        if count >= 1:
+            atk_skill = 'f10'
+        else:
+            atk_skill = 'f6'
 
         # 如果找到怪物A图像
         if monster_a_location is not None:
             # 按下黑洞爆發
-            pyautogui.keyDown('f6');pyautogui.keyUp('f6')
+            pyautogui.keyDown(atk_skill);pyautogui.keyUp(atk_skill)
             time.sleep(0.5) # 等待一段时间，避免连续点击造成问题
             # 点击怪物A图像
             a_x, a_y = pyautogui.center(monster_a_location)
             pyautogui.moveTo(a_x, a_y, 0.5, pyautogui.easeInOutQuad)
             pyautogui.mouseDown(); pyautogui.mouseUp()
             time.sleep(0.5)  # 等待一段时间，避免连续点击造成问题
+            count += 1
 
         # 如果找到怪物B图像
         elif monster_b_location is not None:
             # 按下黑洞爆發
-            pyautogui.keyDown('f6');pyautogui.keyUp('f6')
+            pyautogui.keyDown(atk_skill);pyautogui.keyUp(atk_skill)
             time.sleep(0.5) # 等待一段时间，避免连续点击造成问题
             # 点击怪物A图像
             b_x, b_y = pyautogui.center(monster_b_location)
             pyautogui.moveTo(b_x, b_y, 0.5, pyautogui.easeInOutQuad)
             pyautogui.mouseDown(); pyautogui.mouseUp()
             time.sleep(0.5)  # 等待一段时间，避免连续点击造成问题
+            count += 1
 
         # 如果找到怪物C图像
         elif monster_c_location is not None:
             # 按下黑洞爆發
-            pyautogui.keyDown('f6');pyautogui.keyUp('f6')
+            pyautogui.keyDown(atk_skill);pyautogui.keyUp(atk_skill)
             time.sleep(0.5) # 等待一段时间，避免连续点击造成问题
             # 点击怪物A图像
             c_x, c_y = pyautogui.center(monster_c_location)
             pyautogui.moveTo(c_x, c_y, 0.5, pyautogui.easeInOutQuad)
             pyautogui.mouseDown(); pyautogui.mouseUp()
             time.sleep(0.5)  # 等待一段时间，避免连续点击造成问题
+            count += 1
 
         # 如果找到怪物D图像
         elif monster_d_location is not None:
             # 按下黑洞爆發
-            pyautogui.keyDown('f6');pyautogui.keyUp('f6')
+            pyautogui.keyDown(atk_skill);pyautogui.keyUp(atk_skill)
             time.sleep(0.5) # 等待一段时间，避免连续点击造成问题
             # 点击怪物D图像
             d_x, d_y = pyautogui.center(monster_d_location)
             pyautogui.moveTo(d_x, d_y, 0.5, pyautogui.easeInOutQuad)
             pyautogui.mouseDown(); pyautogui.mouseUp()
             time.sleep(0.5)  # 等待一段时间，避免连续点击造成问题
+            count += 1
 
         else:
-            # 如果屏幕上不再出现(A, B, C)怪物图像，退出循环
-            print('找不到任何場上怪物')
-            break
+            if not_in_fight is not None: #出現了非戰鬥狀態才有的標示
+                print('目前是非戰鬥狀態,退出戰鬥函數')
+                break
 
 切換至目標視窗()
 time.sleep(0.5)
